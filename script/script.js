858038d82da9ea384bccd36527d50c5da8882448
enuu <enuu9923@gmail.com>
2025-11-04 17:28:19 +0900

    11/4

--- script/script.js
+++ script/script.js
@@ -1,7 +1,21 @@
-
-
 document.addEventListener("DOMContentLoaded", function(e){
-
+    headerMenuWrap();
+    menuBtn();
 })
 
+// 헤더 드롭 메뉴
+const headerMenuWrap = () => {
+    $('nav>ul>li').mouseenter(function(){
+        $(this).find('.subMenu').stop().slideDown(300);
+    })
+    $('nav>ul>li').mouseleave(function(){
+        $('.subMenu').stop().slideUp(200);
+    })
+}
 
+// 햄버거 메뉴
+const menuBtn = () => {
+    $('.menuBtn').click(function(){
+        $(this).toggleClass('on');
+    })
+}
